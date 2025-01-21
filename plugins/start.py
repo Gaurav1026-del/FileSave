d9d68eb7b9fd2c7933705a59ad8865ccd6a73317
imakashrabha <165803560+imakashrabha@users.noreply.github.com>
2025-01-21 23:34:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -106,8 +106,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("ғᴏʀ ᴍᴏʀᴇ", url= "https://t.me/Zoroflix"),
-                    InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data= "about")
+                    InlineKeyboardButton("ᴀʙᴏᴜᴛ", callback_data = "about"),
+                    InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close")
                 ]
             ]
         )
